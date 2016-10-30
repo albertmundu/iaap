61ce6cdf7c4722b4ae0598d1afd12c6f2abcc373
albertmunda <albert.adel007@gmail.com>
2016-10-30 16:28:53 +0530

    queue update

--- practice/queue.linkedlist.c
+++ practice/queue.linkedlist.c
@@ -19,7 +19,7 @@ void enqueue(struct node**head,struct node**tail,int data){
         *head=*tail=createnode(data);
     else {
         (*tail)->next=createnode(data);
-        *tail=(*tail->next);
+        *tail=(*tail)->next;
     }
 }
 void dequeue(struct node**head,struct node**tail){
@@ -34,4 +34,39 @@ void dequeue(struct node**head,struct node**tail){
 }
 struct node* front(struct node*head){
     return head;
+}
+int isempty(struct node*head){
+    if(head==NULL)
+        return 1;
+    return 0;
+}
+void prt_queue(struct node*head){
+    for(;head!=NULL;head=head->next)
+    {
+        printf("%d ",head->data);
+    }
+    printf("\n");
+}
+
+int main(){
+    struct node*head=NULL,*tail=NULL;
+    enqueue(&head,&tail,89);
+    enqueue(&head,&tail,89);
+    enqueue(&head,&tail,89);
+    enqueue(&head,&tail,89);
+    enqueue(&head,&tail,89);
+    enqueue(&head,&tail,89);
+    prt_queue(head);
+    dequeue(&head,&tail);
+    dequeue(&head,&tail);
+    dequeue(&head,&tail);
+    dequeue(&head,&tail);
+    dequeue(&head,&tail);
+    prt_queue(head);
+    dequeue(&head,&tail);
+    prt_queue(head);
+    enqueue(&head,&tail,89);
+    enqueue(&head,&tail,89);
+    enqueue(&head,&tail,89);
+    prt_queue(head);
 }
