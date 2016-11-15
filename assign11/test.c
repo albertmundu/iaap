a9eb38cb04af9dbbb90c1929ff2176db511a280b
albertmunda <albert.adel007@gmail.com>
2016-11-15 23:59:29 +0530

    added test.c

--- assign11/test.c
+++ assign11/test.c
@@ -1,58 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define swap(a,b){int t=a;a=b;b=t;} 
 
-typedef struct node {
-    int data;
-    struct node *left;
-    struct node *right;
-    int balanceFactor;
-    int height;
-} node_t;
-
-
-node_t *newnode(int data) {
-    node_t *temp = (node_t *)malloc(sizeof(node_t));
-    temp->data = data;
-    temp->left = NULL;
-    temp->right = NULL;
-    temp->balanceFactor = 0;
-    temp->height = 0;
-    return temp;
-}
-
-node_t *insert(node_t *root, int data) {
-    if(!root)
-        root = newnode(data);
-    else if(data < root->data) {
-        //root->balanceFactor = balanceFactor(root);
-        root->left = insert(root->left, data);
-        root->height = root->left->height + 1;
+void heapify(int *arr,int n,int i){
+    int left=2*i+1;
+    int right=2*i+2;
+    int min=i;
+    if(left <= n && arr[left]<arr[min])
+        min=left;
+    if(right <=n && arr[right]<arr[min])
+        min=right;
+    if(min!=i){
+        swap(arr[min],arr[i]);
+        heapify(arr,n,min);
     }
-    else if(data > root->data) {
-        //root->balanceFactor = balanceFactor(root);
-        root->right = insert(root->right, data);
-        root->height = root->right->height + 1;
-    }
-    return root;
-}
-
-void preOrder(node_t *root) {
-    if(!root) return;
-    printf("%d %d %d\n", root->data, root->height, root->balanceFactor);
-    preOrder(root->left);
-    preOrder(root->right);
-}
-
-int main(int argc, char *argv[]) {
-    node_t *root = NULL;
-    int n, i, data;
-    scanf("%d", &n);
-    int A[n];
-    for(i=0; i<n; ++i) {
-        scanf("%d", &data);
-        A[i] = data;
-        root = insert(root, A[i]);
-    }
-    preOrder(root);
-    return 0;
 }
